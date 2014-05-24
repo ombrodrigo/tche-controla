f44f0ffb88ee0a81d0317d1ea214d84bb2966ec6
Morvana Bonin <morvanabonin@gmail.com>
2014-05-24 09:52:02 -0300

    Daos de Bebida e Pub, não tem FKs.

--- src/com/senac/night_control/database/BebidaDao.java
+++ src/com/senac/night_control/database/BebidaDao.java
@@ -1,5 +1,53 @@
 package com.senac.night_control.database;
 
-public class BebidaDao {
+import com.senac.night_control.model.Bebida;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+public class BebidaDao extends SQLiteOpenHelper {
+	
+	private static final String TABELA = "Bebida";
+	private static final int VERSION = 1;
+	private static final String[] COLUNAS = {"id","bebida_nome"};
+	
+	public BebidaDao(Context context) {
+		super(context, TABELA, null, VERSION);
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+
+		String sql ="CREATE TABLE IF NOT EXISTS " + TABELA +
+		        "( id INTEGER PRIMARY KEY autoincrement," +
+					" nome_bebida TEXT NOT NULL)";
+		
+		     db.execSQL(sql);
+		     
+		     Log.w("Ok", "Database was created");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("Drop Table " + BebidaDao.TABELA);
+	      this.onCreate(db);
+	}
+	
+	public void inserir(Bebida bebida) throws SQLException {
+
+		ContentValues valores = new ContentValues();
+	    	valores.put("nome_bebida", bebida.getBebida_nome());
+	    	
+	    	try {
+	    		getWritableDatabase().insert(TABELA, null, valores);
+	    	} catch (SQLException e) {
+	    		e.printStackTrace();
+	    	} 
+	 }
 
 }
--- src/com/senac/night_control/database/PubDao.java
+++ src/com/senac/night_control/database/PubDao.java
@@ -1,5 +1,53 @@
 package com.senac.night_control.database;
 
-public class PubDao {
+import com.senac.night_control.model.Bebida;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+public class PubDao extends SQLiteOpenHelper {
+	
+	private static final String TABELA = "Bebida";
+	private static final int VERSION = 1;
+	private static final String[] COLUNAS = {"id","pub_nome"};
+	
+	public PubDao(Context context) {
+		super(context, TABELA, null, VERSION);
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+
+		String sql ="CREATE TABLE IF NOT EXISTS " + TABELA +
+		        "( id INTEGER PRIMARY KEY autoincrement," +
+					" pub_bebida TEXT NOT NULL)";
+		
+		     db.execSQL(sql);
+		     
+		     Log.w("Ok", "Database was created");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("Drop Table " + PubDao.TABELA);
+	      this.onCreate(db);
+	}
+	
+	public void inserir(Bebida bebida) throws SQLException {
+
+		ContentValues valores = new ContentValues();
+	    	valores.put("pub_bebida", bebida.getBebida_nome());
+	    	
+	    	try {
+	    		getWritableDatabase().insert(TABELA, null, valores);
+	    	} catch (SQLException e) {
+	    		e.printStackTrace();
+	    	} 
+	 }
 
 }
